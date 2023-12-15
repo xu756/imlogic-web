709887016aefe38e8fbb397ef2d3777ba90f6f6f
xu756 <756334744@qq.com>
2023-12-16 00:53:58 +0800

    滚动

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,18 +1,15 @@
-import { Outlet,useMatches } from 'react-router-dom';
-import { useTitle } from 'ahooks';
+import { Outlet } from 'react-router-dom';
 import Side from '@/components/layout/side/side';
 import Area from '@/components/layout/area/area';
 
 const AuthRoute =()=>{
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const matches:any = useMatches();
-    useTitle(matches[1].handle?.title||import.meta.env.VITE_APP_TITLE||'web');
+
     return(
-        <div className="flex h-full">
+        <div className="flex h-full" >
             <div className="w-16 bg-gray-200 py-4 px-2 ">
                 <Side/>
             </div>
-            <div className="w-52 bg-gray-300 p-4">
+            <div className="w-52 bg-gray-300 ">
                 <Area/>
             </div>
             <div className="flex-1 bg-gray-400 p-4">
