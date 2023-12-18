12873c262ba75cba105fb9c748e9e2cc1d4f1b38
xu756 <756334744@qq.com>
2023-12-18 10:38:41 +0800

    layout bg

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -5,13 +5,13 @@ import Area from '@/components/layout/area/area';
 const AuthRoute = () => {
     return (
         <div className="flex h-full">
-            <div className="w-16 bg-gray-200 py-4 px-2 ">
+            <div className=" w-16 bg-gray-200 py-4 px-2 ">
                 <Side />
             </div>
             <div className="w-52 bg-gray-300 ">
                 <Area />
             </div>
-            <div className="flex-1 bg-gray-400 p-4">
+            <div className="flex-1 bg-blue-400 p-4">
                 <Outlet />
             </div>
         </div>
