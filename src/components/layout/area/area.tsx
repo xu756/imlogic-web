16e094432e481b418e773eb50717105d7ec5629d
xu756 <756334744@qq.com>
2023-12-26 21:58:00 +0800

    chat

--- src/components/layout/area/area.tsx
+++ src/components/layout/area/area.tsx
@@ -20,11 +20,11 @@ const Area = () => {
     // }, 1500);
     return (
         <>
-            <div className=" p-3  w-full flex h-scree ">
+            <div className=" p-4  w-full flex h-scree ">
                 <div className="w-full">
                     <form className="flex items-center">
                         <div className="relative w-full">
-                            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                            <div className="flex absolute inset-y-0 left-0 items-center pl-2 pointer-events-none">
                                 <svg
                                     aria-hidden="true"
                                     className="w-5 h-5 text-gray-500 dark:text-gray-400"
@@ -37,14 +37,14 @@ const Area = () => {
                             </div>
                             <input
                                 type="text"
-                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                className="block w-full pl-6 p-1.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="搜索"
                                 required
                             />
                         </div>
                         <button
                             type="submit"
-                            className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            className="inline-flex items-center py-1.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
                             Search
                         </button>
